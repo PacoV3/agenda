152d1688820436d2f2b754c3c4d2562d034561ff
Francisco Ventura <a173379@une.edu.mx>
2020-06-06 22:28:30 -0500

    Not the last commit

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
-class HerokuConfig(ProductionConfig):
-    @classmethod
-    def init_app(cls, app):
-        ProductionConfig.init_app(app)
-        # log to stderr
-        import logging
-        from logging import StreamHandler
-        file_handler = StreamHandler()
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+# class HerokuConfig(ProductionConfig):
+#     @classmethod
+#     def init_app(cls, app):
+#         ProductionConfig.init_app(app)
+#         # log to stderr
+#         import logging
+#         from logging import StreamHandler
+#         file_handler = StreamHandler()
+#         file_handler.setLevel(logging.INFO)
+#         app.logger.addHandler(file_handler)
